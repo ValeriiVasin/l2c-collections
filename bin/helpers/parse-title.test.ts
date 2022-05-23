b2417bb6eecbe68e2bb4457cc6f5888e33f0a38d
Valerii Vasin <valerii.vasin@gmail.com>
2022-05-23 18:14:40 +0200

    feat: improve enchant parsing

---
+++ bin/helpers/parse-title.test.ts
@@ -0,0 +1,35 @@
+import { describe, test } from '@jest/globals';
+import { parseTitle } from './parse-title';
+
+describe('parse title', () => {
+  test('returns empty string as a name if can not be parsed', () => {
+    expect(parseTitle('')).toEqual({ name: '' });
+  });
+
+  test('title without count and enchant', () => {
+    expect(parseTitle('Составная Костяная Кираса')).toEqual({ name: 'Составная Костяная Кираса' });
+  });
+
+  test('does not return count if its less than 2', () => {
+    expect(parseTitle('Составная Костяная Кираса(1)')).toEqual({ name: 'Составная Костяная Кираса' });
+  });
+
+  test('name with the count', () => {
+    expect(parseTitle('Составная Костяная Кираса(2)')).toEqual({ name: 'Составная Костяная Кираса', count: 2 });
+  });
+
+  test('enchant', () => {
+    expect(parseTitle('+4 Серьга Багрового Полумесяца(1)')).toEqual({
+      name: 'Серьга Багрового Полумесяца',
+      enchant: 4,
+    });
+  });
+
+  test('enchant and count', () => {
+    expect(parseTitle('+3 Серьга Багрового Полумесяца(2)')).toEqual({
+      name: 'Серьга Багрового Полумесяца',
+      enchant: 3,
+      count: 2,
+    });
+  });
+});
