8cbe6c62c62efa8b61c427cba15287b11812a77f
Valerii Vasin <valerii.vasin@gmail.com>
2022-05-29 11:12:21 +0200

    refactor: extract table

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,6 @@
 import classNames from 'classnames/bind';
-import type { Collection } from '../types';
 import styles from './app.module.scss';
-import { CollectionItem } from './components/collection-item/collection-item';
+import { CollectionsTable } from './components/collections-table/collections-table';
 import { FilterInput } from './components/filter-input/filter-input';
 import { Navigation } from './components/navigation/navigation';
 import { Notification } from './components/notification/notification';
@@ -25,39 +24,11 @@ function App() {
         <FilterInput value={query} onChange={(query) => setSearchParams({ query })} />
       </div>
       <Navigation />
-      {collections.length === 0 && <Notification>Ничего не найдено</Notification>}
-      {collections.length > 0 && (
-        <table className={cx('table')}>
-          <thead>
-            <tr>
-              <td>Название</td>
-              <td>Предметы</td>
-              <td>Эффект коллекции</td>
-            </tr>
-          </thead>
-          <tbody>
-            {collections.map((collection) => (
-              <tr data-testid="collection" key={collection.name} className={cx('collection')}>
-                <td className={cx('collection-name')}>{collection.name}</td>
-                <td className={cx('collection-items')}>
-                  <CollectionItemsUi collection={collection} />
-                </td>
-                <td className={cx('collection-effect')}>{collection.effects}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      {collections.length > 0 ? (
+        <CollectionsTable collections={collections} />
+      ) : (
+        <Notification>Ничего не найдено</Notification>
       )}
-    </div>
-  );
-}
-
-function CollectionItemsUi({ collection }: { collection: Collection }) {
-  return (
-    <div className={cx('collection-items-wrapper')}>
-      {collection.items.map((item, index) => (
-        <CollectionItem key={`${collection.name}/${index}`} item={item} />
-      ))}
     </div>
   );
 }
