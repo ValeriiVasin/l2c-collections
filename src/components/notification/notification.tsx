e2ed4740805d4bd47f8f1119854dd9374bd2508f
Valerii Vasin <valerii.vasin@gmail.com>
2022-05-29 09:25:04 +0200

    refactor: extract collection item component

--- src/components/notification/notification.tsx
+++ src/components/notification/notification.tsx
@@ -1,9 +1,9 @@
 import classNames from 'classnames/bind';
-import type { PropsWithChildren } from 'react';
+import type { ReactNode } from 'react';
 import styles from './notification.module.scss';
 const cx = classNames.bind(styles);
 
-export function Notification({ children }: PropsWithChildren<{}>) {
+export function Notification({ children }: { children?: ReactNode }) {
   return (
     <div data-testid="notification" className={cx('notification', 'warning')}>
       {children}
