ac2335bac696a5c2d561c2b8bde80d885e8acc8f
Valerii Vasin <valerii.vasin@gmail.com>
2022-05-29 14:09:31 +0200

    feat: sticky header and focusing input (#22)
    * feat: sticky header
    
    * feat: focus/unfocus search box
    
    * refactor: cleanup app css
    
    * chore: use node 16
    
    * chore: bump deps

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames/bind';
+import { useCallback, useEffect, useRef } from 'react';
 import styles from './app.module.scss';
 import { CollectionsTable } from './components/collections-table/collections-table';
 import { FilterInput } from './components/filter-input/filter-input';
@@ -18,13 +19,45 @@ function App() {
 
   const collections = useCollections({ tab, query });
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [tab]);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const inputFocusHandler = useCallback((event: KeyboardEvent) => {
+    const isInputFocused = document.activeElement === inputRef.current;
+
+    if (isInputFocused && event.code === 'Escape') {
+      inputRef.current?.blur();
+      return;
+    }
+
+    if (!isInputFocused && event.code === 'Slash') {
+      // prevent "/" from being typed inside of input
+      event.preventDefault();
+      inputRef.current?.focus();
+      return;
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('keydown', inputFocusHandler);
+
+    return () => {
+      window.removeEventListener('keydown', inputFocusHandler);
+    };
+  }, [inputFocusHandler]);
+
   return (
-    <div className={cx('content')}>
-      <div className={cx('filter')}>
-        <FilterInput value={query} onChange={(query) => setSearchParams({ query })} />
-      </div>
-      <div className={cx('navigation')}>
-        <Navigation />
+    <div className={cx('content')} data-testid="content">
+      <div className={cx('header')}>
+        <div className={cx('filter')}>
+          <FilterInput ref={inputRef} value={query} onChange={(query) => setSearchParams({ query })} />
+        </div>
+        <div className={cx('navigation')}>
+          <Navigation />
+        </div>
       </div>
       {collections.length > 0 ? (
         <CollectionsTable collections={collections} />
--- src/components/filter-input/filter-input.tsx
+++ src/components/filter-input/filter-input.tsx
@@ -1,11 +1,16 @@
 import classNames from 'classnames/bind';
 import type { DebouncedFunc } from 'lodash';
 import debounce from 'lodash/debounce';
-import { useEffect, useMemo, useRef, useState } from 'react';
+import React, { forwardRef, useEffect, useMemo, useRef, useState } from 'react';
 import styles from './filter-input.module.scss';
 const cx = classNames.bind(styles);
 
-export function FilterInput({ value, onChange }: { value: string; onChange: (value: string) => void }) {
+interface FilterInputProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export const FilterInput = forwardRef<HTMLInputElement, FilterInputProps>(({ value, onChange }, inputRef) => {
   const debouncedRef = useRef<null | DebouncedFunc<(q: string) => void>>(null);
   const [filterText, setFilterText] = useState(value);
 
@@ -23,6 +28,7 @@ export function FilterInput({ value, onChange }: { value: string; onChange: (val
 
   return (
     <input
+      ref={inputRef}
       type="search"
       className={cx('filter-input')}
       value={filterText}
@@ -30,4 +36,4 @@ export function FilterInput({ value, onChange }: { value: string; onChange: (val
       onChange={({ currentTarget }) => setFilterText(currentTarget.value)}
     />
   );
-}
+});
